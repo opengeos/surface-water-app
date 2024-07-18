3272eedab40b058898a5929381a296fd125924f3
Qiusheng Wu <giswqs@gmail.com>
2024-07-18 00:09:53 -0400

    Add monthly history

--- pages/01_jrc.py
+++ pages/01_jrc.py
@@ -29,13 +29,30 @@ class Map(geemap.Map):
         )
 
     def add_buttons(self, position="topright", **kwargs):
-        widget = widgets.VBox()
-        layout = layout = widgets.Layout(width="auto")
-        hist_btn = widgets.Button(description="Show histogram", layout=layout)
-        bar_btn = widgets.Button(description="Show bar chart", layout=layout)
+        padding = "0px 5px 0px 5px"
+        widget = widgets.VBox(layout=widgets.Layout(padding=padding))
+        layout = widgets.Layout(width="auto")
+        style = {"description_width": "initial"}
+        hist_btn = widgets.Button(description="Occurrence", layout=layout)
+        bar_btn = widgets.Button(description="Monthly history", layout=layout)
         reset_btn = widgets.Button(description="Reset", layout=layout)
-        scale = widgets.IntSlider(min=30, max=1000, value=90, description="Scale")
-        widget.children = [widgets.HBox([hist_btn, bar_btn, reset_btn]), scale]
+        scale = widgets.IntSlider(
+            min=30, max=1000, value=90, description="Scale", layout=layout, style=style
+        )
+        month_slider = widgets.IntRangeSlider(
+            description="Months",
+            value=[5, 10],
+            min=1,
+            max=12,
+            step=1,
+            layout=layout,
+            style=style,
+        )
+        widget.children = [
+            widgets.HBox([hist_btn, bar_btn, reset_btn]),
+            month_slider,
+            scale,
+        ]
         self.add_widget(widget, position=position, **kwargs)
         output = widgets.Output()
         self.add_widget(output, position="bottomleft", add_header=False)
@@ -43,12 +60,14 @@ class Map(geemap.Map):
         def hist_btn_click(b):
             region = self.user_roi
             if region is not None:
+                output.clear_output()
+                output.append_stdout("Computing histogram...")
                 image = ee.Image("JRC/GSW1_4/GlobalSurfaceWater").select(["occurrence"])
                 self.default_style = {"cursor": "wait"}
                 hist = geemap.image_histogram(
                     image,
                     region,
-                    scale=60,
+                    scale=scale.value,
                     height=350,
                     width=550,
                     x_label="Water Occurrence (%)",
@@ -59,12 +78,50 @@ class Map(geemap.Map):
                     },
                     return_df=False,
                 )
-                output.clear_output()
+
                 with output:
+                    output.clear_output()
                     display(hist)
                 self.default_style = {"cursor": "default"}
+            else:
+                output.clear_output()
+                with output:
+                    output.append_stdout("Please draw a region of interest first.")
 
         hist_btn.on_click(hist_btn_click)
+
+        def bar_btn_click(b):
+            region = self.user_roi
+            if region is not None:
+                self.default_style = {"cursor": "wait"}
+                output.clear_output()
+                output.append_stdout("Computing monthly history...")
+                bar = geemap.jrc_hist_monthly_history(
+                    region=region,
+                    scale=scale.value,
+                    height=350,
+                    width=550,
+                    layout_args={
+                        "title": dict(x=0.5),
+                        "margin": dict(l=0, r=0, t=10, b=0),
+                    },
+                    frequency="month",
+                    start_month=month_slider.value[0],
+                    end_month=month_slider.value[1],
+                    denominator=1e4,
+                    y_label="Area (ha)",
+                )
+
+                with output:
+                    output.clear_output()
+                    display(bar)
+                self.default_style = {"cursor": "default"}
+            else:
+                output.clear_output()
+                with output:
+                    output.append_stdout("Please draw a region of interest first.")
+
+        bar_btn.on_click(bar_btn_click)
 
         def reset_btn_click(b):
             self._draw_control.clear()
