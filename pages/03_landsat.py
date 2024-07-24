53ef1d36ff2cbfef7822aa9cc5aa26255193d9dd
Qiusheng Wu <giswqs@gmail.com>
2024-07-23 21:56:43 -0400

    Update web app

--- pages/03_landsat.py
+++ pages/03_landsat.py
@@ -4,6 +4,7 @@ import geemap
 import ipywidgets as widgets
 from IPython.display import display
 import solara
+from datetime import date
 
 
 class Map(geemap.Map):
@@ -11,6 +12,23 @@ class Map(geemap.Map):
         super().__init__(**kwargs)
         self.add_basemap("Esri.WorldImagery")
         self.add_gui_widget(add_header=True)
+
+    def clean_up(self):
+
+        layers = [
+            "Pre-event Image",
+            "Post-event Image",
+            "Pre-event NDWI",
+            "Post-event NDWI",
+            "Pre-event Water",
+            "Post-event Water",
+            "Disappeared Water",
+            "New Water",
+        ]
+        for layer_name in layers:
+            layer = self.find_layer(layer_name)
+            if layer is not None:
+                self.remove(layer)
 
     def add_gui_widget(self, position="topright", **kwargs):
 
@@ -22,11 +40,13 @@ class Map(geemap.Map):
         padding = "0px 5px 0px 5px"
         pre_start_date = widgets.DatePicker(
             description="Start",
+            value=date(2014, 1, 1),
             style=style,
             layout=widgets.Layout(padding=padding, width="160px"),
         )
         pre_end_date = widgets.DatePicker(
             description="End",
+            value=date(2014, 12, 31),
             style=style,
             layout=widgets.Layout(padding=padding, width="160px"),
         )
@@ -50,11 +70,13 @@ class Map(geemap.Map):
         ]
         post_start_date = widgets.DatePicker(
             description="Start",
+            value=date(2024, 1, 1),
             style=style,
             layout=widgets.Layout(padding=padding, width="160px"),
         )
         post_end_date = widgets.DatePicker(
             description="End",
+            value=date(2024, 12, 31),
             style=style,
             layout=widgets.Layout(padding=padding, width="160px"),
         )
@@ -78,16 +100,52 @@ class Map(geemap.Map):
         ]
 
         apply_btn = widgets.Button(description="Apply", layout=layout)
-        split_btn = widgets.Button(description="Split", layout=layout)
         reset_btn = widgets.Button(description="Reset", layout=layout)
-        close_btn = widgets.Button(description="Close", layout=layout)
-        buttons = widgets.HBox([apply_btn, split_btn, reset_btn, close_btn])
+        buttons = widgets.HBox([apply_btn, reset_btn])
         output = widgets.Output()
 
-        widget.children = [pre_widget, post_widget, buttons, output]
+        use_split = widgets.Checkbox(
+            value=False,
+            description="Split map",
+            style=style,
+            layout=widgets.Layout(padding=padding, width="100px"),
+        )
+
+        use_ndwi = widgets.Checkbox(
+            value=False,
+            description="Compute NDWI",
+            style=style,
+            layout=widgets.Layout(padding=padding, width="160px"),
+        )
+
+        ndwi_threhold = widgets.FloatSlider(
+            description="Threshold",
+            min=-1,
+            max=1,
+            value=0,
+            step=0.05,
+            readout=True,
+            style=style,
+            layout=widgets.Layout(padding=padding, width="230px"),
+        )
+
+        options = widgets.HBox(
+            [
+                use_split,
+                use_ndwi,
+                ndwi_threhold,
+            ]
+        )
+
+        widget.children = [pre_widget, post_widget, options, buttons, output]
         self.add_widget(widget, position=position, **kwargs)
 
         def apply_btn_click(b):
+
+            marker_layer = self.find_layer("Search location")
+            if marker_layer is not None:
+                self.remove(marker_layer)
+            self.clean_up()
 
             if self.user_roi is None:
                 output.clear_output()
@@ -105,33 +163,108 @@ class Map(geemap.Map):
                 output.clear_output()
                 output.append_stdout("Computing... Please wait.")
                 roi = ee.FeatureCollection(self.user_roi)
-                pre_col = (
-                    ee.ImageCollection("NASA/HLS/HLSL30/v002")
-                    .filterBounds(roi)
-                    .filterDate(
-                        pre_start_date.value.strftime("%Y-%m-%d"),
-                        pre_end_date.value.strftime("%Y-%m-%d"),
+                vis_params = {"bands": ["B6", "B5", "B4"], "min": 0, "max": 0.4}
+                if pre_start_date.value.strftime("%Y-%m-%d") < "2013-04-11":
+                    pre_col = geemap.landsat_timeseries(
+                        roi,
+                        start_year=pre_start_date.value.year,
+                        end_year=pre_end_date.value.year,
+                    ).select(["SWIR1", "NIR", "Red", "Green"], ["B6", "B5", "B4", "B3"])
+                else:
+                    pre_col = (
+                        ee.ImageCollection("NASA/HLS/HLSL30/v002")
+                        .filterBounds(roi)
+                        .filterDate(
+                            pre_start_date.value.strftime("%Y-%m-%d"),
+                            pre_end_date.value.strftime("%Y-%m-%d"),
+                        )
+                        .filter(ee.Filter.lt("CLOUD_COVERAGE", pre_cloud_cover.value))
                     )
-                    .filter(ee.Filter.lt("CLOUD_COVERAGE", pre_cloud_cover.value))
-                )
-                post_col = (
-                    ee.ImageCollection("NASA/HLS/HLSL30/v002")
-                    .filterBounds(roi)
-                    .filterDate(
-                        post_start_date.value.strftime("%Y-%m-%d"),
-                        post_end_date.value.strftime("%Y-%m-%d"),
+
+                if post_start_date.value.strftime("%Y-%m-%d") < "2013-04-11":
+                    post_col = geemap.landsat_timeseries(
+                        roi,
+                        start_year=post_start_date.value.year,
+                        end_year=post_end_date.value.year,
+                    ).select(["SWIR1", "NIR", "Red", "Green"], ["B6", "B5", "B4", "B3"])
+                else:
+                    post_col = (
+                        ee.ImageCollection("NASA/HLS/HLSL30/v002")
+                        .filterBounds(roi)
+                        .filterDate(
+                            post_start_date.value.strftime("%Y-%m-%d"),
+                            post_end_date.value.strftime("%Y-%m-%d"),
+                        )
+                        .filter(ee.Filter.lt("CLOUD_COVERAGE", post_cloud_cover.value))
                     )
-                    .filter(ee.Filter.lt("CLOUD_COVERAGE", post_cloud_cover.value))
-                )
 
                 pre_img = pre_col.median().clip(roi)
                 post_img = post_col.median().clip(roi)
-                vis_params = {"bands": ["B6", "B5", "B4"], "min": 0, "max": 0.4}
-                self.add_layer(pre_img, vis_params, "Pre-event Image")
-                self.add_layer(post_img, vis_params, "Post-event Image")
+
+                if use_split.value:
+                    left_layer = geemap.ee_tile_layer(
+                        pre_img, vis_params, "Pre-event Image"
+                    )
+                    right_layer = geemap.ee_tile_layer(
+                        post_img, vis_params, "Post-event Image"
+                    )
+                    self.split_map(
+                        left_layer,
+                        right_layer,
+                        add_close_button=True,
+                        left_label="Pre-event",
+                        right_label="Post-event",
+                    )
+                else:
+                    pre_img = pre_col.median().clip(roi)
+                    post_img = post_col.median().clip(roi)
+                    self.add_layer(pre_img, vis_params, "Pre-event Image")
+                    self.add_layer(post_img, vis_params, "Post-event Image")
+
+                if use_ndwi.value and (not use_split.value):
+                    pre_ndwi = pre_img.normalizedDifference(["B3", "B6"]).rename("NDWI")
+                    post_ndwi = post_img.normalizedDifference(["B3", "B6"]).rename(
+                        "NDWI"
+                    )
+                    ndwi_vis = {"min": -1, "max": 1, "palette": "ndwi"}
+                    self.add_layer(pre_ndwi, ndwi_vis, "Pre-event NDWI", False)
+                    self.add_layer(post_ndwi, ndwi_vis, "Post-event NDWI", False)
+
+                    pre_water = pre_ndwi.gt(ndwi_threhold.value)
+                    post_water = post_ndwi.gt(ndwi_threhold.value)
+                    self.add_layer(
+                        pre_water.selfMask(), {"palette": "blue"}, "Pre-event Water"
+                    )
+                    self.add_layer(
+                        post_water.selfMask(), {"palette": "red"}, "Post-event Water"
+                    )
+                    new_water = post_water.subtract(pre_water).gt(0)
+                    disappear_water = pre_water.subtract(post_water).gt(0)
+                    self.add_layer(
+                        disappear_water.selfMask(),
+                        {"palette": "brown"},
+                        "Disappeared Water",
+                    )
+                    self.add_layer(
+                        new_water.selfMask(), {"palette": "cyan"}, "New Water"
+                    )
+
+                    with output:
+                        output.clear_output()
+
                 output.clear_output()
 
         apply_btn.on_click(apply_btn_click)
+
+        def reset_btn_click(b):
+            self.clean_up()
+            self._draw_control.clear()
+            draw_layer = self.find_layer("Drawn Features")
+            if draw_layer is not None:
+                self.remove(draw_layer)
+            output.clear_output()
+
+        reset_btn.on_click(reset_btn_click)
 
 
 @solara.component
